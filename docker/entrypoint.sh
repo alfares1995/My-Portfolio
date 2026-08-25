c948a8099c38ac279c17cd2b5255410a4d505fe5
alfares1995 <alfarisodai@gmail.com>
2026-08-25 19:45:31 +0100

    fix: allow nginx to serve persistent uploads

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,7 @@ mkdir -p storage/framework/cache storage/framework/sessions storage/framework/vi
     storage/logs storage/app/public storage/app/database bootstrap/cache
 [ -f storage/app/database/database.sqlite ] || touch storage/app/database/database.sqlite
 chown -R www-data:www-data storage bootstrap/cache
+chmod -R a+rX storage/app/public
 
 php artisan config:cache
 php artisan route:cache
