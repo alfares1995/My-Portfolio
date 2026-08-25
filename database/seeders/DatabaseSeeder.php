717c2e6dc29689537f8fd378ae58bbc230fd9fc4
alfares1995 <alfarisodai@gmail.com>
2026-08-25 18:22:48 +0100

    fix: seed production admin without Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::updateOrCreate(['email' => 'alfarisodai@gmail.com'], [
             'name' => 'Odai Alfares',
-            'email' => 'alfarisodai@gmail.com',
+            'password' => Hash::make('ChangeThisAdminPassword'),
             'is_admin' => true,
         ]);
     }
